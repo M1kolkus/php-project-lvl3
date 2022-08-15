4907870dbf57fe480e720e2df7907e50c30bf95e
Torchyk Mikolai <M1kolikus@yandex.by>
2022-08-15 11:26:30 +0300

    step 1

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AnalyzerController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,4 +18,4 @@ use Illuminate\Support\Facades\Route;
 //    return view('welcome');
 //});
 
-Route::get('/', ['AnalyzerController', 'analyzer']);
+Route::get('/', [AnalyzerController::class, 'analyzer']);
